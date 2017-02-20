c8761b97f6aba29a8bc4ef7b2c863fc5d0e646ad
Luiz Gustavo L Moura <lglmoura@gmail.com>
2017-02-20 20:16:34 -0300

    Criando Botao no MainActivity

---
+++ app/src/main/java/br/edu/iff/pooa20162/contatoappnovo/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package br.edu.iff.pooa20162.contatoappnovo.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import br.edu.iff.pooa20162.contatoappnovo.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void chamaTelaContatos(View v){
+        Toast.makeText(this,"",Toast.LENGTH_SHORT);
+    }
+}
